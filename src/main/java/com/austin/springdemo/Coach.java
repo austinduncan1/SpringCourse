d6be52879afa5b80a734db7197ca621754611765
Austin Duncan <austinduncan@Austins-MacBook-Pro.local>
2022-11-19 10:54:32 -0500

    removing old project structure

--- src/main/java/com/austin/springdemo/Coach.java
+++
@@ -1,6 +0,0 @@
-package com.austin.springdemo;
-
-public interface Coach {
-	public String getDailyWorkout();
-	public String getDailyFortune();
-}
--- src/main/java/com/austin/springdemo/services/FortuneService.java
+++
@@ -1,5 +0,0 @@
-package com.austin.springdemo.services;
-
-public interface FortuneService {
-	public String getFortune();
-}
